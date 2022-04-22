03acfa5e53a45148f90d502762abeeebf4e5e71a
Vanessa-Vidal <vanessa.vidal@kellogg.northwestern.edu>
2022-04-22 21:00:40 +0000

    version3

--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -27,6 +27,16 @@ bitcoin = gets.chomp
 
 # 2. The value will be a string, so you'll want to convert it to a Float.
 bitcoin = bitcoin.to_f
+puts "You have #{bitcoin} bitcoin"
+
 
 # 3. inspect the bitcoin_data hash
-# puts bitcoin_data
+puts bitcoin_data
+
+usd_value=bitcoin_data["bpi"]["USD"]["rate_float"]
+puts "1 Bitcoin is valued at $ #{usd_value} USD"
+
+
+total_value = usd_value * bitcoin
+puts "You have a total value of #{total_value} USD worth of Bitcoin"
+
